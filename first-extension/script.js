2449b03a9775cf88abccfef45181642dfeb59b6f
Michael Caplan <michael.caplan@henryschein.com>
2018-09-21 22:56:37 -0300

    reorged

--- first-extension/script.js
+++ first-extension/script.js
@@ -1,10 +1,27 @@
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-   chrome.declarativeContent.onPageChanged.addRules([{
-     conditions: [new chrome.declarativeContent.PageStateMatcher({
-       pageUrl: {schemes: ['https', 'http', 'localhost', 'chrome', 'file']},
-     })
-     ],
-         actions: [new chrome.declarativeContent.ShowPageAction()]
-   }]);
- });
+chrome.declarativeContent.onPageChanged.removeRules(undefined, addRules);
 
+function addRules() {
+
+    chrome.declarativeContent.onPageChanged.addRules(
+        [{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        schemes: ['https', 'http', 'localhost', 'chrome', 'file']
+                    },
+                })
+            ],
+            actions: [
+                new chrome.declarativeContent.ShowPageAction()
+            ]
+        }]
+    );
+}
+
+
+// chrome.pageAction.onClicked.addListener(onClickedListener);
+//
+//
+// function onClickedListener() {
+//
+// }
--- first-extension_solution/script.js
+++ first-extension_solution/script.js
@@ -1,18 +1,47 @@
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-   chrome.declarativeContent.onPageChanged.addRules([{
-     conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {schemes: ['https', 'http', 'localhost', 'chrome', 'file']},
-     })
-     ],
-         actions: [new chrome.declarativeContent.ShowPageAction()]
-   }]);
- });
+chrome.declarativeContent.onPageChanged.removeRules(undefined, addRules);
 
-chrome.pageAction.onClicked.addListener(function() {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{file:  'main.js'
-		});
-	});
-});
+function addRules() {
+
+    chrome.declarativeContent.onPageChanged.addRules(
+        [{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        schemes: ['https', 'http', 'localhost', 'chrome', 'file']
+                    },
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]
+    );
+}
+
+
+chrome.pageAction.onClicked.addListener(onClickedListener);
+
+
+function onClickedListener() {
+
+    chrome.tabs.query(
+        {
+            active: true,
+            currentWindow: true
+        },
+        function (tabs) {
+
+            // chrome.tabs.executeScript(
+            //     tabs[0].id,
+            //     {
+            //         code: 'document.body.style.backgroundColor = "green";'
+            //     }
+            // );
+
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {
+                    file:  'main.js'
+                }
+            );
+        }
+    );
+}
